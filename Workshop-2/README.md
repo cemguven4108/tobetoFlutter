27d5c471fd3ee2d9ae3297e73951d4235a58bf86
Cem Güven <cemguven4108@gmail.com>
2023-10-26 16:02:30 +0300

    Workshop-2 added

---
+++ Workshop-2/README.md
@@ -0,0 +1,7 @@
+Workshop 26.10.2023
+
+//ToDo - Check if a given number is Armstrong number
+
+//Info - Armstrong number is a number that is equal to the sum of cubes of its digits.
+
+ 0, 1, 153, 370, 371 and 407 
---
+++ Workshop-2/armstrongCalculator.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+void main() {
+  int n = 12;
+  bool result = checkIfArmstrong(n);
+  print(result);
+}
+
+bool checkIfArmstrong(int nbr) {
+  if (nbr == computeDigits(getDigits(nbr)))
+    return true;
+  return false;
+}
+
+List<int> getDigits(int nbr) {
+  List<int> list = [];
+
+  while (nbr != 0) {
+    list.add((nbr % 10));
+    nbr = (nbr / 10).floor();
+  }
+  return list;
+}
+
+int computeDigits(List<int> digits) {
+  int total, l;
+
+  l = digits.length;
+  total = 0;
+  for (int i = 0; i < l; i++) {
+    total += pow(digits[i], l) as int;
+  }
+
+  return total;
+}
